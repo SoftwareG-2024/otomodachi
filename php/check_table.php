57d547d43e506c4af1e967a6c4c94473814bc468
tohihiro oka <okatoshi1521@gmail.com>
2024-07-27 12:16:23 +0900

    管理画面のデザイン更新

--- php/check_table.php
+++ php/check_table.php
@@ -1,12 +1,28 @@
-<?php
-$db = new SQLite3('../data/budget.db');
 
-$result = $db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='expenses'");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>データベースの確認</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/button.css">
+</head>
+<body>
+    <h1>家計簿データ</h1>
+    <?php
+    $db = new SQLite3('../data/budget.db');
 
-if ($result->fetchArray()) {
-    echo "テーブル 'expenses' が存在します";
-} else {
-    echo "テーブル 'expenses' が存在しません";
-}
-echo "<a href='../index.html'>back</a>";
-?>
+    $result = $db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='expenses'");
+
+    if ($result->fetchArray()) {
+        echo "テーブル 'expenses' が存在します";
+    } else {
+        echo "テーブル 'expenses' が存在しません";
+    }
+    ?>
+
+    <div class='button-group'>
+        <button onclick="window.location.href='../index.html';" class="button">Back</button>
+    </div>
+</body>
+</html>
--- php/count.php
+++ php/count.php
@@ -1,17 +1,33 @@
-<?php
-try {
-    $db = new SQLite3('../data/budget.db');
-} catch (Exception $e) {
-    echo "エラー: " . $e->getMessage();
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>データベースの確認</title>
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
+    <link rel="stylesheet" type="text/css" href="../css/button.css">
+</head>
+<body>
+    <h1>家計簿データ</h1>
 
-$results = $db->query("
-    SELECT STRFTIME('%Y-%m', date) AS month, COUNT(*) AS count
-    FROM expenses
-    GROUP BY STRFTIME('%Y-%m', date)
-");
+    <?php
+    try {
+        $db = new SQLite3('../data/budget.db');
+    } catch (Exception $e) {
+        echo "エラー: " . $e->getMessage();
+    }
 
-while ($row = $results->fetchArray()) {
-    echo "月: " . $row['month'] . ", 件数: " . $row['count'] . "<br>";
-}
-?>
+    $results = $db->query("
+        SELECT STRFTIME('%Y-%m', date) AS month, COUNT(*) AS count
+        FROM expenses
+        GROUP BY STRFTIME('%Y-%m', date)
+    ");
+
+    while ($row = $results->fetchArray()) {
+        echo "月: " . $row['month'] . ", 件数: " . $row['count'] . "<br>";
+    }
+    ?>    
+    <div class='button-group'>
+        <button onclick="window.location.href='../index.html';" class="button">Back</button>
+    </div>
+</body>
+</html>
